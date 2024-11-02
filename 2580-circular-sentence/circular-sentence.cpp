49d50eb70322204731f0e0729c4846142965d061
Muhammad Abdullah Khan <162717503+abdullah0805@users.noreply.github.com>
2024-11-02 17:56:22 +0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (83.91%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -4,29 +4,16 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        vector<char> letters;
-        int words = 0;
-        for(int i = 0;sentence[i] != '\0';i++){
-            if(i == 0){
-                letters.push_back(sentence[i]);
-            }
-            else if(sentence[i] == ' '){
-                letters.push_back(sentence[i - 1]); 
-                letters.push_back(sentence[i + 1]);     
-                words++;          
-            }
-            else if(sentence[i+1] == '\0'){
-                letters.push_back(sentence[i]);
-                words++;
-            }
-        }
-        if(letters[0] != letters[letters.size() - 1]){
+        if(sentence[0] != sentence[sentence.length() - 1]){
             return false;
         }
-        for(int i = 0;i < letters.size();i+=2){
-            if(i + 2 < letters.size() && letters[i + 1] != letters[i + 2]){
-                return false;
+        for(int i = 0;sentence[i] != '\0';i++){
+            if(sentence[i] == ' '){
+                if(sentence[i + 1] != sentence[i - 1]){
+                    return false;
+                }  
             }
+
         }
         return true;
     }
